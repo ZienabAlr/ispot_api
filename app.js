baf0e6c39655e48b3fb8e1359f7ba773a66dbade
ZienabAlr <r0784494@student.thomasmore.be>
2023-05-30 13:13:09 +0200

    feat(app.js): cors has been implemented

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors ()); 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use ('/api/v1/test', testRouter);
